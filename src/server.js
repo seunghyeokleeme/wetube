7ee198d82bc0b6c14f53c82a793ed1f684e6196b
seunghyeokleeme <seunghyeok.tech@gmail.com>
2023-06-07 15:56:59 +0900

    3.11 External Middlewares

--- src/server.js
+++ src/server.js
@@ -1,19 +1,14 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
 
-const handleHome = (req, res) => {
-  return res.send({ message: "Home" });
-};
+const handleHome = (req, res) => res.send("Home");
 
-const handleLogin = (req, res) => {
-  return res.send("Login here");
-};
-
+app.use(morgan("dev"));
 app.get("/", handleHome);
-app.get("/login", handleLogin);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
