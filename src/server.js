0d22ac2aa21d155fb8f9b53df15fe08d38913109
seunghyeokleeme <seunghyeok.tech@gmail.com>
2023-06-07 14:45:41 +0900

    GET Requests and Responses

--- src/server.js
+++ src/server.js
@@ -4,6 +4,17 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = (req, res) => {
+  return res.send({ message: "Home" });
+};
+
+const handleLogin = (req, res) => {
+  return res.send("Login here");
+};
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
 
