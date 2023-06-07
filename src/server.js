89ace3f3f3c9a79b35da9e0e4dc3f1a0b415ebd4
seunghyeokleeme <seunghyeok.tech@gmail.com>
2023-06-07 13:08:39 +0900

    You First Server

---
+++ src/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const handleListening = () =>
+  console.log(`Server listening on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
