f8e3ef156115f1f1c687cbf8c866f653432e0a6d
seunghyeokleeme <seunghyeok.tech@gmail.com>
2023-06-22 15:27:26 +0900

    5.2 Partials

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,6 @@
 export const trending = (req, res) => res.render("home");
-export const edit = (req, res) => res.send("Edit Video");
-export const see = (req, res) => {
-  const { id } = req.params;
-  return res.send(`${id}번 video 페이지입니다.`);
-};
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Searching by...");
 export const remove = (req, res) => res.send("해당 비디오를 삭제합니다.");
 export const upload = (req, res) => res.send("비디오 업로드 페이지입니다.");
--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ const app = express();
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
+
 app.use(morgan("dev"));
 app.use("/", globalRouter);
 app.use("/users", userRouter);
