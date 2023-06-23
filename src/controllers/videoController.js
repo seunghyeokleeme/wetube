ecb29423d9bf88c44966feb5901e1bb6c2eca7e7
seunghyeokleeme <seunghyeok.tech@gmail.com>
2023-06-23 15:57:10 +0900

    6.0 Array Database part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,33 +1,40 @@
-export const trending = (req, res) => {
-  const videos = [
-    {
-      id: 1,
-      title: "First Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-    },
-    {
-      id: 2,
-      title: "Second Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-    },
-    {
-      id: 3,
-      title: "Third Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-    },
-  ];
-  return res.render("home", { pageTitle: "Home", videos });
+let videos = [
+  {
+    id: 1,
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+  },
+  {
+    id: 2,
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+  },
+  {
+    id: 3,
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+  },
+];
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", videos });
+export const see = (req, res) => {
+  const { id } = req.params;
+  const video = videos.find((video) => video.id === +id);
+  if (video) {
+    return res.render("watch", { pageTitle: `Watching ${video.title}` });
+  } else {
+    return res.redirect("/");
+  }
 };
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Searching by...");
 export const remove = (req, res) => res.send("해당 비디오를 삭제합니다.");
