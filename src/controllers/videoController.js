31ffa9ab59990e376456bd941d6a9138e24f7d50
seunghyeokleeme <seunghyeok.tech@gmail.com>
2023-06-23 14:07:56 +0900

    5.8 Iteration

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,7 @@
-const fakeUser = {
-  username: "Nicolas",
-  loggedIn: false,
+export const trending = (req, res) => {
+  const videos = [];
+  return res.render("home", { pageTitle: "Home", videos });
 };
-
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser });
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Searching by...");
