7d93f1720daf49ecf4d590fd3d1919e2a39711d1
seunghyeokleeme <seunghyeok.tech@gmail.com>
2023-06-23 13:08:52 +0900

    5.4 Variables to Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,6 @@
-export const trending = (req, res) => res.render("home");
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Searching by...");
 export const remove = (req, res) => res.send("해당 비디오를 삭제합니다.");
 export const upload = (req, res) => res.send("비디오 업로드 페이지입니다.");
