0c9d73405c11e9f1297bb33a5d86f074f860af15
seunghyeokleeme <seunghyeok.tech@gmail.com>
2023-06-30 22:48:20 +0900

    각종 에러 화면 pageTitle 추가

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -14,19 +14,27 @@ export const errorHandler = (err, req, res, next) => {
   console.error(err.message);
 
   if (err instanceof ValidationError) {
-    return res.status(400).render(err.view, { errorMessage: err.message });
+    return res
+      .status(400)
+      .render(err.view, { errorMessage: err.message, pageTitle: err.view });
   }
 
   if (err instanceof UnauthorizedError) {
-    return res.status(401).render(err.view, { errorMessage: err.message });
+    return res
+      .status(401)
+      .render(err.view, { errorMessage: err.message, pageTitle: err.view });
   }
 
   if (err instanceof ForbiddenError) {
-    return res.status(403).render(err.view, { errorMessage: err.message });
+    return res
+      .status(403)
+      .render(err.view, { errorMessage: err.message, pageTitle: err.view });
   }
 
   if (err instanceof NotFoundError) {
-    return res.status(404).render("404", { errorMessage: err.message });
+    return res
+      .status(404)
+      .render("404", { errorMessage: err.message, pageTitle: err.view });
   }
 
   return res.status(err.status || 500).render("500");
