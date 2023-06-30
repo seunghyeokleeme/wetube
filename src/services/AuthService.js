f4ede1479e8ac555c3d97321eaf64833e8c83eb1
seunghyeokleeme <seunghyeok.tech@gmail.com>
2023-06-30 22:45:19 +0900

    AuthService.js 생성

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,22 +1,42 @@
 import { ValidationError } from "../errors";
 
-const SNSConfig = {
+const snsConfigurations = {
   github: {
-    baseUrl: "https://github.com/login/oauth/authorize",
-    config: {
-      client_id: "c25822a49e162d9e12c3",
+    authURL: "https://github.com/login/oauth/authorize",
+    tokenURL: "https://github.com/login/oauth/access_token",
+    authParameters: {
+      client_id: process.env.GH_CLIENT,
       allow_signup: false,
       scope: "read:user user:email",
+    },
+    tokenParameters: {
+      client_id: process.env.GH_CLIENT,
+      client_secret: process.env.GH_SECRET,
     },
   },
 };
 
-export const getLoginUrl = (snsType) => {
-  const snsConfig = SNSConfig[snsType];
+export const getAuthorizationURL = (snsType) => {
+  const snsConfig = snsConfigurations[snsType];
+
   if (!snsConfig) {
     throw new ValidationError("알 수 없는 SNS Type입니다.", "login");
   }
-  const params = new URLSearchParams(snsConfig.config).toString();
-  const finalUrl = `${snsConfig.baseUrl}?${params}`;
-  return finalUrl;
+
+  const params = new URLSearchParams(snsConfig.authParameters).toString();
+  const authURL = `${snsConfig.authURL}?${params}`;
+
+  return authURL;
+};
+
+export const getAccessTokenURL = (snsType, code) => {
+  const snsConfig = snsConfigurations[snsType];
+  if (!snsConfig) {
+    throw new ValidationError("알 수 없는 SNS Type입니다.", "login");
+  }
+  const tokenParameters = { ...snsConfig.tokenParameters, code };
+  const params = new URLSearchParams(tokenParameters).toString();
+  const tokenURL = `${snsConfig.tokenURL}?${params}`;
+
+  return tokenURL;
 };
