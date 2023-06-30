3f8d0b04d7f000409b04a75d3477d51c9a6cd8e1
seunghyeokleeme <seunghyeok.tech@gmail.com>
2023-06-30 13:51:15 +0900

    7.12 MongoStore

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -21,6 +22,7 @@ app.use(
     secret: "Hello!",
     resave: true,
     saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/devtube" }),
   })
 );
 
