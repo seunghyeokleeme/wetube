6a984be49ae64aa29ee36714ceae0cb11419e1a8
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2023-07-03 12:42:53 +0900

    fix(Router): logout url 라우터 변경
    Before: /logout
    After: /users/logout
    
    Refs: #1

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,14 +1,13 @@
 import express from "express";
 import { home, search } from "../controllers/videoController";
 import { getJoin, getLogin } from "../controllers/userController";
-import { logout, postLogin } from "../controllers/authController";
+import { postLogin } from "../controllers/authController";
 
 const globalRouter = express.Router();
 
 globalRouter.get("/", home);
 globalRouter.get("/join", getJoin);
 globalRouter.route("/login").get(getLogin).post(postLogin);
-globalRouter.route("/logout").get(logout);
 globalRouter.get("/search", search);
 
 export default globalRouter;
