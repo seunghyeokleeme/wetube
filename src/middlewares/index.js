0b74c765fe31e32cd4d89a85e14cae9a2dc99c09
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2023-07-06 20:17:14 +0900

    [feature] Protector and Public Middlewares (#11)
    * feat(middleware): privateOnly 와 publicOnly 미들웨어 생성
    
    Refs: #10
    
    * feat(router): 모든 route publicOnly, privateOnly 미적용
    
    Refs: #10
    
    * fix(error): 서버측 에러(500번대) 만 출력
    
    Close #10

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -11,8 +11,6 @@ export const handleNotFound = (req, res, next) => {
 };
 
 export const errorHandler = (err, req, res, next) => {
-  console.error(err.message);
-
   if (err instanceof ValidationError) {
     return res
       .status(400)
@@ -36,7 +34,7 @@ export const errorHandler = (err, req, res, next) => {
       .status(404)
       .render("404", { errorMessage: err.message, pageTitle: err.view });
   }
-
+  console.error(err.message); // Todo: 500번대, 그외 error 만 서버에 로그
   return res.status(err.status || 500).render("500");
 };
 
@@ -44,5 +42,19 @@ export const locals = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Devtube";
   res.locals.loggedInUser = req.session.user ?? {};
+  next();
+};
+
+export const privateOnly = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    return res.redirect("/login");
+  }
+  next();
+};
+
+export const publicOnly = (req, res, next) => {
+  if (req.session.loggedIn) {
+    return res.redirect("/");
+  }
   next();
 };
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -2,12 +2,13 @@ import express from "express";
 import { home, search } from "../controllers/videoController";
 import { getJoin, getLogin } from "../controllers/userController";
 import { postLogin } from "../controllers/authController";
+import { publicOnly } from "../middlewares";
 
 const globalRouter = express.Router();
 
 globalRouter.get("/", home);
-globalRouter.get("/join", getJoin);
-globalRouter.route("/login").get(getLogin).post(postLogin);
+globalRouter.route("/join").all(publicOnly).get(getJoin);
+globalRouter.route("/login").all(publicOnly).get(getLogin).post(postLogin);
 globalRouter.get("/search", search);
 
 export default globalRouter;
