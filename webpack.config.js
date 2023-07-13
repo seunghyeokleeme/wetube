355db87a4f2d83c9e2c732085cd871952905ce17
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2023-07-13 12:28:29 +0900

    feat: Webpack Configuration part

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/client/js/main.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "assets", "js"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
+  },
+};
