2c9df12da0919e13ff779e6f048c714db39267bc
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2023-07-14 20:39:05 +0900

    Feature/webpack (#20)
    * feat: Webpack Configuration part
    
    * feat: SCSS Loader
    
    * feat: MiniCssExtractPlugin
    
    * feat: DE 개선
    
    nodemon 설정 및 webpack watch, clean 설정

--- src/server.js
+++ src/server.js
@@ -33,6 +33,7 @@ app.use(
 
 app.use(locals);
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("assets"));
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/client/js/main.js",
+  watch: true,
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/styles.css",
+    }),
+  ],
+  output: {
+    filename: "js/main.js",
+    path: path.resolve(__dirname, "assets"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
